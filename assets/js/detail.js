f7a8d80a6dda635d7b1df8b52c9674147585859b
Brigoliboon <95647084+Brigoliboon@users.noreply.github.com>
2024-03-16 19:06:14 +0800

    cover 3

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -74,7 +74,7 @@ fetchDataFromServer(
     movieDetail.classList.add("movie-detail");
     movieDetail.innerHTML = `
                 <head>
-                <meta property= "og:url" content="${imageBaseURL}" />
+                <meta property= "schema:url" content="${imageBaseURL}" />
                 </head>
                 <div id="overlay" onclick="off()">
                 <center><div class="movie-body">
